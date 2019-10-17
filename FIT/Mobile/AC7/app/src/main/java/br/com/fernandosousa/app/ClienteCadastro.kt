bba7b7f291ac5f1a4a3a5180816c610a6c0205d3
joao vitor paulino martins <paulino.joaovitor@yahoo.com.br>
2019-10-17 20:40:42 -0300

    Initial commit

---
+++ FIT/Mobile/AC7/app/src/main/java/br/com/fernandosousa/app/ClienteCadastro.kt
@@ -0,0 +1,24 @@
+package br.com.fernandosousa.lmsapp
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_cliente_cadastro.*
+
+class ClienteCadastro : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_cliente_cadastro)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener {
+            view ->
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show()
+        }
+    }
+
+}
